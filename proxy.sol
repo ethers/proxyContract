7220318e64aff9d8db0726bea41d458822e6be6f
ethers <ethereum@outlook.com>
2016-01-07 17:43:58 -0800

    value of type address works

--- proxy.sol
+++ proxy.sol
@@ -24,7 +24,7 @@ contract Proxy {
 
 	function forward_method(address _destination, uint _value, bytes4 _methodName, bytes32[] _transactionData) public returns (uint) {
 		if (msg.sender == owner) {
-		    bytes4 method = bytes4(sha3("register(bytes32,bytes32)"));
+		    bytes4 method = bytes4(sha3("register(bytes32,address)"));
 		    _destination.call.value(_value)(method, _transactionData);
 
 // 			_destination.call.value(_value)(_methodName, _transactionData);
@@ -36,16 +36,16 @@ contract Proxy {
 }
 
 contract SimpleRegistry {
-    mapping (bytes32 => bytes32) registry;
+    mapping (bytes32 => address) registry;
 
-    function register(bytes32 key, bytes32 value) {
+    function register(bytes32 key, address value) {
         registry[key] = value;
     }
 
-    function lookup(bytes32 key) returns (bytes32) {
+    function lookup(bytes32 key) returns (address) {
         return registry[key];
     }
 }
 
 // args for Solidity browser
-// "0xaddressOfRegistry", 0, 0, ["0xcde", "0x456"]
+// "0x692a70d2e424a56d2c6c27aa97d1a86395877b3a", 0, 0, ["0xcde", "0x1234567890123456789012345678901234567890"]
