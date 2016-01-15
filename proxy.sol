264c8c292a8e46d2692887894f37da1db66a827c
ethers <ethereum@outlook.com>
2016-01-15 01:54:26 -0800

    registry value created with bytes32[] workaround, but entries are all 0, suggesting bug in web3 or Geth (since Solidity browser is ok)

--- proxy.sol
+++ proxy.sol
@@ -22,10 +22,10 @@ contract Proxy {
 		}
 	}
 
-	function forward_method(address _destination, uint _gas, uint _value, bytes4 _methodName, bytes32[] _transactionData) public returns (uint) {
+	function forward_method(bytes32[] _transactionData, address _destination, uint _value, bytes4 _methodName) public returns (uint) {
 		/*if (msg.sender == owner) {*/
 		    bytes4 method = bytes4(sha3("register(bytes32,address)"));
-		    _destination.call.gas(_gas).value(_value)(method, _transactionData);
+		    _destination.call.value(_value)(method, _transactionData);
 
 // 			_destination.call.value(_value)(_methodName, _transactionData);
 			/*Forwarded(_destination, _value);*/
@@ -55,11 +55,11 @@ contract SimpleRegistry {
 
 /*
 
-var proxyContract = web3.eth.contract([{"constant":true,"inputs":[],"name":"owner","outputs":[{"name":"","type":"address"}],"type":"function"},{"constant":true,"inputs":[],"name":"funcSig","outputs":[{"name":"","type":"bytes4"}],"type":"function"},{"constant":false,"inputs":[{"name":"_transactionData","type":"bytes32[]"},{"name":"_destination","type":"address"},{"name":"_gas","type":"uint256"},{"name":"_value","type":"uint256"},{"name":"_methodName","type":"bytes4"}],"name":"forward_method","outputs":[{"name":"","type":"uint256"}],"type":"function"},{"inputs":[],"type":"constructor"}]);
+var proxyContract = web3.eth.contract([{"constant":false,"inputs":[{"name":"_transactionData","type":"bytes32[]"},{"name":"_destination","type":"address"},{"name":"_value","type":"uint256"},{"name":"_methodName","type":"bytes4"}],"name":"forward_method","outputs":[{"name":"","type":"uint256"}],"type":"function"},{"constant":true,"inputs":[],"name":"owner","outputs":[{"name":"","type":"address"}],"type":"function"},{"constant":false,"inputs":[{"name":"_destination","type":"address"},{"name":"_value","type":"uint256"},{"name":"_transactionBytecode","type":"bytes"}],"name":"forward","outputs":[],"type":"function"},{"constant":false,"inputs":[{"name":"_new_owner","type":"address"}],"name":"transfer_ownership","outputs":[],"type":"function"},{"inputs":[],"type":"constructor"},{"anonymous":false,"inputs":[{"indexed":false,"name":"_destination","type":"address"},{"indexed":false,"name":"_value","type":"uint256"}],"name":"Forwarded","type":"event"},{"anonymous":false,"inputs":[{"indexed":false,"name":"_new_owner","type":"address"}],"name":"TransferOwnership","type":"event"}]);
 var proxy = proxyContract.new(
    {
      from: web3.eth.accounts[0],
-     data: '60606040525b33600060006101000a81548173ffffffffffffffffffffffffffffffffffffffff021916908302179055505b6101fb8061003f6000396000f360606040526000357c0100000000000000000000000000000000000000000000000000000000900480638da5cb5b1461004f578063cdb4f17514610088578063f9682bc0146100ab5761004d565b005b61005c6004805050610136565b604051808273ffffffffffffffffffffffffffffffffffffffff16815260200191505060405180910390f35b610095600480505061015c565b6040518082815260200191505060405180910390f35b6101206004808035906020019082018035906020019191908080602002602001604051908101604052809392919081815260200183836020028082843782019150505050505090909190803590602001909190803590602001909190803590602001909190803590602001909190505061018b565b6040518082815260200191505060405180910390f35b600060009054906101000a900473ffffffffffffffffffffffffffffffffffffffff1681565b600060149054906101000a90047c01000000000000000000000000000000000000000000000000000000000281565b60006113577c010000000000000000000000000000000000000000000000000000000002600060146101000a81548163ffffffff02191690837c010000000000000000000000000000000000000000000000000000000090040217905550600190506101f2565b9594505050505056',
+     data: '60606040525b33600060006101000a81548173ffffffffffffffffffffffffffffffffffffffff021916908302179055505b61050d8061003f6000396000f360606040526000357c01000000000000000000000000000000000000000000000000000000009004806314df20591461005a5780638da5cb5b146100dc578063d7f31eb914610115578063f0350c041461017d57610058565b005b6100c6600480803590602001908201803590602001919190808060200260200160405190810160405280939291908181526020018383602002808284378201915050505050509090919080359060200190919080359060200190919080359060200190919050506103f6565b6040518082815260200191505060405180910390f35b6100e96004805050610195565b604051808273ffffffffffffffffffffffffffffffffffffffff16815260200191505060405180910390f35b61017b6004808035906020019091908035906020019091908035906020019082018035906020019191908080601f0160208091040260200160405190810160405280939291908181526020018383808284378201915050505050509090919050506102c4565b005b61019360048080359060200190919050506101bb565b005b600060009054906101000a900473ffffffffffffffffffffffffffffffffffffffff1681565b3073ffffffffffffffffffffffffffffffffffffffff163373ffffffffffffffffffffffffffffffffffffffff1614806102425750600060009054906101000a900473ffffffffffffffffffffffffffffffffffffffff1673ffffffffffffffffffffffffffffffffffffffff163373ffffffffffffffffffffffffffffffffffffffff16145b156102c05780600060006101000a81548173ffffffffffffffffffffffffffffffffffffffff021916908302179055507fcfaaa26691e16e66e73290fc725eee1a6b4e0e693a1640484937aac25ffb55a481604051808273ffffffffffffffffffffffffffffffffffffffff16815260200191505060405180910390a15b5b50565b600060009054906101000a900473ffffffffffffffffffffffffffffffffffffffff1673ffffffffffffffffffffffffffffffffffffffff163373ffffffffffffffffffffffffffffffffffffffff1614156103f0578273ffffffffffffffffffffffffffffffffffffffff168282604051808280519060200190808383829060006004602084601f0104600f02600301f150905090810190601f1680156103805780820380516001836020036101000a031916815260200191505b5091505060006040518083038185876185025a03f192505050507f6f1deddfc28100c291fae8f1064e4a91e844f0841993bb8fba9a913c3b801d808383604051808373ffffffffffffffffffffffffffffffffffffffff1681526020018281526020019250505060405180910390a15b5b505050565b6000600060405180807f726567697374657228627974657333322c616464726573732900000000000000815260200150601901905060405180910390207c01000000000000000000000000000000000000000000000000000000008091040290508473ffffffffffffffffffffffffffffffffffffffff1684827c010000000000000000000000000000000000000000000000000000000090049088604051837c0100000000000000000000000000000000000000000000000000000000028152600401808280519060200190602002808383829060006004602084601f0104600f02600301f15090500191505060006040518083038185886185025a03f193505050505060019150610504565b5094935050505056',
      gas: 3000000
    }, function(e, contract){
     console.log(e, contract);
@@ -68,14 +68,14 @@ var proxy = proxyContract.new(
     }
  })
 
-pAr='0xbdddc684156a3ead02625ecbccbb7d24b5e43e82'
+pAr='0xf3a202c6c95c8ea2982edbe387b774c0f6637ccb'
 
 pcon=proxyContract.at(pAr)
 eth.defaultAccount = eth.coinbase
 dest='0x5a63738e866969b29989bfb97df6307b1f5602d2'
 val = 0
-pcon.forward_method.sendTransaction(["te", 5], dest, 1900000, val, 0, {gas:2100000})
-pcon.forward_method.call(["te", 5], dest, 1900000, val, 0, {gas:2100000})
+pcon.forward_method.sendTransaction(["te", 5], dest, val, 0, {gas:2100000})
+pcon.forward_method.call(["te", 5], dest, val, 0, {gas:2100000})
 
 
 Registering on created registry
